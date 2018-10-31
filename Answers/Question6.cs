7045c9909c0d279eb02aea33c6189ee8db0cd860
Zac Keskin <zackeskin@outlook.com>
2018-10-31 23:32:45 +0000

    Showcase Q6 Answer
    Basic Dijkstra's Algorithm. Could be improved using a priority queue/heap implementation.

--- Answers/Question6.cs
+++ Answers/Question6.cs
@@ -1,11 +1,36 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+using System;
+//using System.Collections.Generic;
+using Dijkstra.NET.Extensions;
+using Dijkstra.NET.Model;
+
+namespace credit_suisse
 {
-    public class Question6
+
+
+public class Question6
     {
+        
         public static int Answer(int numOfServers, int targetServer, int[,] connectionTimeMatrix)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            // Initialise new Graph
+            var graph = new Graph<int, string>();
+
+            for(int i=0; i<numOfServers; i++)
+                {graph.AddNode(i);}
+            
+            // Populate Graph With Adjacency Matrix
+            for(int i=0; i<numOfServers; i++)
+                {
+                for(uint j=0; j<numOfServers; j++)
+                    graph.Connect(Convert.ToUInt32(i), j, connectionTimeMatrix[i,j], "n/a");
+                    
+                }
+
+            // Calculate shortest route
+            ShortestPathResult result = graph.Dijkstra(0,Convert.ToUInt32(targetServer)); //result contains the shortest path
+
+            return result.Distance;
         }
+
     }
 }
