dc4cad41acea0a7e22b0d02741fcba02754d2f3e
Zac Keskin <zackeskin@outlook.com>
2018-10-31 23:31:50 +0000

    Showcase Q5 Answer
    This is a classic coin-change problem. The greedy algorithm is not guaranteed to work, and the computational complexity is high without a dynamic-programming approach.
    
    For this reason, I attempt a dynamic programming solution, although note this appears less efficient than the method outlined by Johnsonbaugh and Schaefer in Algorithms (see: http://condor.depaul.edu/rjohnson/algorithm/coins.pdf)

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -1,11 +1,43 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+using System;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question5
     {
-        public static int Answer(int[] numOfShares, int totalValueOfShares)
-        {
-            //TODO: Please work out the solution;
-            return -1;
-        }
+
+       public static int Answer(int[] numOfShares, int totalValueOfShares)
+        { 
+            // Use dynamic programming solution to account for
+            // overlapping subproblems
+
+            // We work our way up to totalValueOfShares, reusing calculations 
+            // from the factors to find the smallest path from 0 to totalValueOfShares
+
+            // Create a table 
+            int[] table = new int[totalValueOfShares + 1]; 
+        
+            // Default to totalValueOfShares = 0
+            table[0] = 0; 
+        
+
+            for (int i=1; i<=totalValueOfShares; i++) 
+            { 
+                // Initialise all to MaxInt
+                table[i] = int.MaxValue;
+                
+                for (int j=0; j<numOfShares.Length; j++) 
+                if (numOfShares[j] <= i) 
+                { 
+                    int sub_result = table[i - numOfShares[j]]; 
+                    if (sub_result != int.MaxValue &&  
+                        sub_result + 1 < table[i]) 
+                        table[i] = sub_result + 1; 
+                } 
+            } 
+            if (table[totalValueOfShares] == int.MaxValue)
+                {table[totalValueOfShares] = 0;}
+            return table[totalValueOfShares]; 
+            
+        } 
     }
 }
