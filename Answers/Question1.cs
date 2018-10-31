cdae8ca6d11eb8b042cc9220bef7a5fe22d74ec6
Zac Keskin <zackeskin@outlook.com>
2018-10-31 23:17:08 +0000

    Showcase Q1 answer
    Bitwise XOR, making use of commutativity to avoid unnecessary calculations. (Upper triangular 'adjacency matrix')

--- Answers/Question1.cs
+++ Answers/Question1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
@@ -8,10 +8,25 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question1
     {
-        public static int Answer(int[] portfolios)
+       public static int Answer(int[] portfolios) 
         {
-            //TODO: Please work out the solution;
-            return -1;
+            int max_xor = 0;
+            int xor = 0;
+            int N = portfolios.Length;
+
+            // Loop over all combinations
+            for(int i=0; i<N; i++){
+                for (int j=i+1; j<N; j++)
+                    {
+                    //compare if xor is > answer
+                    xor  = portfolios[i] ^ portfolios[j];     
+                    if (xor > max_xor)
+                        {max_xor = xor;}
+                    }
+
+                }
+            return max_xor;
         }
     }
+    
 }
